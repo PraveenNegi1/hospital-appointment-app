a6924ef980716fb85e428796c2673b5d9f30676a
PraveenNegi1 <praveennegi700@gmail.com>
2026-01-15 12:04:56 +0530

    done

--- app/auth/layout.js
+++
@@ -1,8 +0,0 @@
-// app/(auth)/layout.js
-export default function AuthLayout({ children }) {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500">
-      {children}
-    </div>
-  );
-}
