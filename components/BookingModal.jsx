fa25fbc45b5229e85b40d3f94e1a39acd1caa086
PraveenNegi1 <praveennegi700@gmail.com>
2026-01-20 14:47:57 +0530

    done

--- components/BookingModal.jsx
+++ components/BookingModal.jsx
@@ -276,7 +276,6 @@ export default function BookingModal({ doctor, onClose }) {
             </div>
           </div>
 
-          {/* Reason */}
           <div>
             <label className="block text-sm font-semibold text-gray-700 mb-2">
               Reason for Visit *
