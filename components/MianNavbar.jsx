a369f16c785d7a214e4eb1205387df93fc5f0a7a
PraveenNegi1 <praveennegi700@gmail.com>
2026-01-27 16:49:15 +0530

    done

--- components/MianNavbar.jsx
+++ components/MianNavbar.jsx
@@ -36,6 +36,7 @@ export default function MainNavbar() {
       <nav className="sticky top-0 z-50 font-serif bg-white/80 backdrop-blur-md border-b border-gray-200 shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
           {/* Logo */}
+          
           <Link href="/" className="flex items-center gap-2.5">
             <HeartPulse className="w-8 h-8 text-indigo-600" />
             <span className="text-2xl font-extrabold text-gray-900">
